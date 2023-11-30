82ebe6255d820043a7d88e2784da25cf7c99dab2
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 14:04:40 +0100

    showing blog entries

--- src/pages/PageBlog.tsx
+++ src/pages/PageBlog.tsx
@@ -4,6 +4,21 @@ import { AppContext } from "../AppContext";
 export const PageBlog = () => {
 	const { blogItems } = useContext(AppContext);
 	return (
-		<p>There are {blogItems.length} blog entries.</p>
+		<>
+		<h2 className="text-xl mb-4">There are {blogItems.length} blog entries.</h2>
+
+
+			{blogItems.map(blogItem => {
+				return (
+					<div key={blogItem.id} className="bg-slate-600 p-4 mb-4 rounded">
+						<p className="text-gray-300 text-xl">{blogItem.title}</p>
+						<p className="text-yellow-300 mb-2">{blogItem.when}</p>
+						<p className="text-gray-200">{blogItem.body}</p>
+					</div>
+				)
+			})}
+
+
+		</>
 	)
 }
