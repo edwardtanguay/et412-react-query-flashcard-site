25b2bc44ea7c6ce57ab67f0e838f3ee5bb96955c
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 14:12:46 +0100

    posts in

--- src/pages/PageBlog.tsx
+++ src/pages/PageBlog.tsx
@@ -11,7 +11,7 @@ export const PageBlog = () => {
 			{blogItems.map(blogItem => {
 				return (
 					<div key={blogItem.id} className="bg-slate-600 p-4 mb-4 rounded">
-						<p className="text-gray-300 text-xl">{blogItem.title}</p>
+						<p className="text-gray-100 text-xl">{blogItem.title}</p>
 						<p className="text-yellow-300 mb-2">{blogItem.when}</p>
 						<p className="text-gray-200">{blogItem.body}</p>
 					</div>
