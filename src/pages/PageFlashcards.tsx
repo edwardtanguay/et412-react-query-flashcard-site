d05a9dea5523a6233acf4e6775b12cd26be02dd6
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 11:29:10 +0100

    react query loads ok

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,30 +1,42 @@
-import { useEffect, useState } from "react";
 import { IFlashcard } from "../interfaces";
 import { getFlashcards } from "../dataModel/flashcardModel";
+import { useQuery } from "@tanstack/react-query";
+import { wait } from "../tools";
 
 export const PageFlashcards = () => {
-	const [flashcards, setFlashcards] = useState<IFlashcard[]>([]);
+	const flashcardsQuery = useQuery<IFlashcard[]>({
+		queryKey: ["flashcards"],
+		queryFn: () => wait(2000).then( () => getFlashcards()),
+	});
 
-	useEffect(() => {
-		(async () => {
-			const _flashcards = await getFlashcards();
-			setFlashcards(_flashcards);
-		})();
-	}, []);
+	if (flashcardsQuery.isLoading) {
+		return <p>Loading...</p>
+	}
 
 	return (
 		<>
-			<h2 className="text-xl mb-4">This is the flashcards page.</h2>
-			<div className="">
-				{flashcards.map((flashcard) => {
-					return (
-						<div key={flashcard.id} className="bg-blue-300 p-2 rounded w-fit mb-4">
-							<p className="font-bold">{flashcard.front}</p>
-							<p className="italic">{flashcard.back}</p>
-						</div>
-					);
-				})}
-			</div>
+			{flashcardsQuery.data && (
+				<>
+					<h2 className="text-xl mb-4">
+						This is the flashcards page.
+					</h2>
+					<div className="">
+						{flashcardsQuery.data.map((flashcard) => {
+							return (
+								<div
+									key={flashcard.id}
+									className="bg-blue-300 p-2 rounded w-fit mb-4"
+								>
+									<p className="font-bold">
+										{flashcard.front}
+									</p>
+									<p className="italic">{flashcard.back}</p>
+								</div>
+							);
+						})}
+					</div>
+				</>
+			)}
 		</>
 	);
 };
