0e4e67d9d04fa48a16fe07747b1ca5fd59bb1810
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 11:18:40 +0100

    main.tsx setup for React Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { router } from "./AppRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<RouterProvider router={router} />
+	<QueryClientProvider client={queryClient}>
+		<RouterProvider router={router} />
+		<ReactQueryDevtools />
+	</QueryClientProvider>
 );
