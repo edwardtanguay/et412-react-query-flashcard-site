a8236d37f566fed8907bc588c63ce7c79a295693
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 11:02:31 +0100

    flashcards styled

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { IFlashcard } from "../interfaces";
 import axios from "axios";
 
-const flashcardsUrl = 'http://localhost:3011/flashcards';
+const flashcardsUrl = "http://localhost:3011/flashcards";
 
 export const PageFlashcards = () => {
 	const [flashcards, setFlashcards] = useState<IFlashcard[]>([]);
@@ -10,22 +10,25 @@ export const PageFlashcards = () => {
 	const loadFlashcards = async () => {
 		const response = await axios.get(flashcardsUrl);
 		setFlashcards(response.data);
-	}
+	};
 
 	useEffect(() => {
 		loadFlashcards();
-	}, [])
+	}, []);
 
 	return (
 		<>
-		<h2 className="text-xl">This is the flashcards page.</h2>
-			<div>
-				{flashcards.map(flashcard => {
+			<h2 className="text-xl mb-4">This is the flashcards page.</h2>
+			<div className="">
+				{flashcards.map((flashcard) => {
 					return (
-						<p>{flashcard.front}</p>
-					)
+						<div className="bg-blue-300 p-2 rounded w-fit mb-4">
+							<p className="font-bold">{flashcard.front}</p>
+							<p className="italic">{flashcard.back}</p>
+						</div>
+					);
 				})}
-		</div>
+			</div>
 		</>
-	)
-}
+	);
+};
