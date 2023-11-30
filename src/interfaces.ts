958e704c7b4042fd099f3b95160994d1b4c8c7e9
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-30 10:30:20 +0100

    flashcards load

---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IFlashcard {
+	id: number;
+	front: string;
+	back: string;
+}
--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import { IFlashcard } from "../interfaces";
+import axios from "axios";
+
+const flashcardsUrl = "http://localhost:3011/flashcards";
+
 export const PageFlashcards = () => {
-	return (
-		<p>This is the flashcards page.</p>
-	)
-}
+	const [flashcards, setFlashcards] = useState<IFlashcard[]>([]);
+
+	const loadFlashcards = async () => {
+		const response = await axios.get(flashcardsUrl);
+		setFlashcards(response.data);
+	};
+
+	useEffect(() => {
+		loadFlashcards();
+	}, []);
+
+	return <p>There are {flashcards.length} flashcards.</p>;
+};
